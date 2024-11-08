8ae5c4da51d44fe99b7f2678fef4974033975809
exttreme0001 <fpm.LukashovES@bsu.by>
2024-11-08 19:47:13 +0300

    design, first step

--- CustS/main/views.py
+++ CustS/main/views.py
@@ -1,4 +1,11 @@
 
 from django.shortcuts import render
 def home (request):
-    return render(request,"main/layout.html")
+    images = [
+        'main/img/person.png',
+        'main/img/person.png',
+        'main/img/person.png',
+        'main/img/person.png',
+        'main/img/person.png'
+    ]
+    return render(request,"main/mainpage.html",{'images': images})
