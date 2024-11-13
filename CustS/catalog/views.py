c90a0191fe4114e74615ecef8cc8bd9933faf2ea
LordVillain <danyailyinchyk2005@gmail.com>
2024-11-13 16:13:34 +0300

    Разделил все файлы views.py на services.py и views.py для разделения работы с базой данных и запросами пользователей соотвественно

--- CustS/catalog/views.py
+++ CustS/catalog/views.py
@@ -9,3 +9,5 @@ def catalog(request):
 def product_detail(request, product_id):
     product = get_product_by_id(product_id)
     return render(request, 'catalog/product_detail.html', {'product': product})
+
+
