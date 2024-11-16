bf4f723dbe7a66a5fb7ab9479490612825d01da3
LordVillain <danyailyinchyk2005@gmail.com>
2024-11-16 20:47:53 +0300

    Добавил корзину на главную сраницу сайта, добавил carts.css

--- CustS/carts/urls.py
+++ CustS/carts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart/', views.carts, name='carts'),
+    path('', views.carts, name='carts'),
     path('add/<int:product_id>/', views.add, name='add'),
     path('change/<int:product_id>/', views.change, name='change'),
     path('remove/<int:product_id>/', views.remove, name='remove'),
