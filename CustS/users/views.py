15557aca9856ef16a24538abe50870033f160a5d
exttreme0001 <fpm.LukashovES@bsu.by>
2024-11-17 19:08:48 +0300

    добавлен декоратор переадресации,профиль пользователя

--- CustS/users/views.py
+++ CustS/users/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render, redirect
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.contrib import auth
 from django.urls import reverse
-from users.forms import UserLoginForm,UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm,UserRegistrationForm
 from .services import register_user, authenticate_user, logout_user
-
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('user:login')
+            user=form.instance
+            auth.login(request,user)
+            messages.success(request,f"{user}, Вы успешно зарегистрировались и вошли в аккаунт!")
+            return HttpResponseRedirect('home')
     else:
         form = UserRegistrationForm()
 
@@ -29,7 +35,11 @@ def user_login(request):
             user = auth.authenticate(username=username,password=password)
             if user:
                 auth.login(request,user)
-                return redirect('home')
+                messages.success(request,f"{username}, Вы вошли в аккаунт!")
+
+                if request.POST.get('next',None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                return HttpResponseRedirect('home')
     else:
         form = UserLoginForm()
     context = {
@@ -38,12 +48,27 @@ def user_login(request):
     }
     return render(request,'users/login.html',context)
 
+@login_required
 def user_logout(request):
+    messages.success(request,f"{request.user.username}, Вы вышли из аккаунта!")
     auth.logout(request)
-    return redirect(reverse('home'))  # Перенаправление на главную страницу после выхода
+    all_messages = messages.get_messages(request)
+    for message in all_messages:
+        print(message)
+    return HttpResponseRedirect(reverse('home'))  # Перенаправление на главную страницу после выхода
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST,instance=request.user,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Профиль успешно обновлён!")
+            return HttpResponseRedirect('user:profile')
+    else:
+        form = ProfileForm(instance=request.user)
     context = {
-        'title': ',mainpage'
+        'title': ',mainpage',
+        'form':form,
     }
     return render(request,'users/profile.html',context)
