035e24a2f0952590521913a533da2a143d864188
LordVillain <danyailyinchyk2005@gmail.com>
2024-11-23 21:01:13 +0300

    улучшил админ панель

--- CustS/catalog/admin.py
+++ CustS/catalog/admin.py
@@ -6,15 +6,27 @@ class VideoInline(admin.TabularInline):  # Можно использовать a
     model = Video
     extra = 1  # Количество пустых полей для добавления новых видео
 
-# Регистрируем модель Product с добавлением инлайновой модели Video
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    inlines = [VideoInline]
-
-# Регистрируем модель Category
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name",]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "quantity", "price", "discount"]
+    list_editable = ["discount",]
+    search_fields = ["name", "description"]
+    list_filter = ["discount", "quantity", "category"]
+    fields = [
+        "name",
+        "category",
+        "slug",
+        "description",
+        "image",
+        ("price", "discount"),
+        "quantity",
+    ]
 
 from .models import SubCategory, SubClass
 
